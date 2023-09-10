8d9bd867544606851448c8f1882387861064c641
yelee park <dev.yelee@gmail.com>
2023-09-10 18:20:12 +0900

    feat : 팀 생성 로직 구현

--- src/App.js
+++ src/App.js
@@ -1,15 +1,50 @@
 import {
   Grid,
   Typography,
-  TextField,
   Container,
   Button,
-  Stack,
   Box,
+  Paper,
+  TextField,
+  Stack,
   Chip,
 } from "@mui/material";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
 
 function App() {
+  const { register, handleSubmit, reset } = useForm();
+
+  const [teams, setTeams] = useState([]);
+
+  const createTeams = (people, teamCount) => {
+    if (teamCount <= 0 || people.length === 0) {
+      // TODO: Error
+      return false;
+    }
+
+    const shuffledPeople = [...people].sort(() => Math.random() - 0.5);
+
+    const teams = Array.from({ length: teamCount }, () => []);
+    shuffledPeople.forEach((person, index) => {
+      const teamIndex = index % teamCount;
+      teams[teamIndex].push(person);
+    });
+
+    setTeams(teams);
+  };
+  const onSubmit = (data) => {
+    const list = data?.participantList
+      .split("\n")
+      .filter((item) => item !== "");
+    const numberOfTeams = data?.numberOfTeams;
+    createTeams(list, numberOfTeams);
+  };
+  const handleReset = () => {
+    reset({ numberOfTeams: "", participantList: "" });
+    setTeams([]);
+  };
+
   return (
     <Container
       sx={{
@@ -18,42 +53,70 @@ function App() {
       }}
       maxWidth={`lg`}
     >
-      <Grid container spacing={4}>
-        <Grid item xs={12} md={12}>
-          <Typography variant="h1">Title</Typography>
+      <form onSubmit={handleSubmit(onSubmit)} action="">
+        <Grid container spacing={4}>
+          <Grid item xs={12} md={12} textAlign={`center`}>
+            <Typography variant="h1" fontWeight={`bold`}>
+              Random Team Generator
+            </Typography>
+          </Grid>
+          <Grid item xs={12} md={6}>
+            <Paper elevation={2} sx={{ p: 10 }}>
+              <Stack>
+                <TextField
+                  type={`number`}
+                  label={`Number of Teams`}
+                  placeholder={"Enter the number of teams"}
+                  sx={{ mb: 2 }}
+                  InputLabelProps={{ shrink: true }}
+                  inputProps={{ min: 0 }}
+                  {...register(`numberOfTeams`, {
+                    min: 2,
+                    required: true,
+                  })}
+                />
+                <TextField
+                  label={`Participant List`}
+                  multiline
+                  rows={10}
+                  name={`participantList`}
+                  placeholder="Enter participant names (separated by Enter)"
+                  InputLabelProps={{ shrink: true }}
+                  {...register(`participantList`, { required: true })}
+                />
+              </Stack>
+            </Paper>
+          </Grid>
+          <Grid item xs={12} md={6}>
+            <Box>
+              <Button
+                variant="contained"
+                size={`large`}
+                type={`submit`}
+                color={`primary`}
+              >
+                Generate
+              </Button>
+              <Button
+                onClick={handleReset}
+                variant="contained"
+                size={`large`}
+                color={`secondary`}
+              >
+                Reset
+              </Button>
+              {teams.map((team, index) => (
+                <Box key={index} display={`flex`}>
+                  <Typography>Team {index + 1}</Typography>
+                  {team.map((participant) => (
+                    <Chip key={participant} label={participant} />
+                  ))}
+                </Box>
+              ))}
+            </Box>
+          </Grid>
         </Grid>
-        <Grid item xs={12} md={6}>
-          <Box>
-            <Stack>
-              <TextField
-                type={`number`}
-                label={`팀 count`}
-                placeholder="팀 count"
-                defaultValue={4}
-                sx={{ mb: 2 }}
-                InputLabelProps={{ shrink: true }}
-                inputProps={{ min: 0 }}
-              />
-              <TextField
-                label={`팀 명단`}
-                multiline
-                placeholder="팀 명단 입력"
-                InputLabelProps={{ shrink: true }}
-              />
-            </Stack>
-            <Button variant="contained" sx={{ mt: 2 }}>
-              Generate
-            </Button>
-          </Box>
-        </Grid>
-        <Grid item xs={12} md={6}>
-          <Box sx={{ border: `1px dotted pink` }}>
-            <Chip label={`123`} />
-            <Chip label={`456`} />
-            <Chip label={`789`} />
-          </Box>
-        </Grid>
-      </Grid>
+      </form>
     </Container>
   );
 }
