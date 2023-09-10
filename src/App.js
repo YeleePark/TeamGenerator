24af8e71af4c70f0615400f413f4435eedfac616
yelee park <dev.yelee@gmail.com>
2023-09-10 09:06:31 +0900

    chore : 레이아웃 구현을 위한 초기 HTML

--- src/App.js
+++ src/App.js
@@ -1,5 +1,61 @@
+import {
+  Grid,
+  Typography,
+  TextField,
+  Container,
+  Button,
+  Stack,
+  Box,
+  Chip,
+} from "@mui/material";
+
 function App() {
-  return <div className="App">hi</div>;
+  return (
+    <Container
+      sx={{
+        backgroundColor: `background.default`,
+        height: `100vh`,
+      }}
+      maxWidth={`lg`}
+    >
+      <Grid container spacing={4}>
+        <Grid item xs={12} md={12}>
+          <Typography variant="h1">Title</Typography>
+        </Grid>
+        <Grid item xs={12} md={6}>
+          <Box>
+            <Stack>
+              <TextField
+                type={`number`}
+                label={`팀 count`}
+                placeholder="팀 count"
+                defaultValue={4}
+                sx={{ mb: 2 }}
+                InputLabelProps={{ shrink: true }}
+                inputProps={{ min: 0 }}
+              />
+              <TextField
+                label={`팀 명단`}
+                multiline
+                placeholder="팀 명단 입력"
+                InputLabelProps={{ shrink: true }}
+              />
+            </Stack>
+            <Button variant="contained" sx={{ mt: 2 }}>
+              Generate
+            </Button>
+          </Box>
+        </Grid>
+        <Grid item xs={12} md={6}>
+          <Box sx={{ border: `1px dotted pink` }}>
+            <Chip label={`123`} />
+            <Chip label={`456`} />
+            <Chip label={`789`} />
+          </Box>
+        </Grid>
+      </Grid>
+    </Container>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
+import { createTheme, CssBaseline, ThemeProvider } from "@mui/material";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+const theme = createTheme();
+
 root.render(
-  <React.StrictMode>
+  <ThemeProvider theme={theme}>
+    <CssBaseline />
     <App />
-  </React.StrictMode>,
+  </ThemeProvider>,
 );
 
 // If you want to start measuring performance in your app, pass a function
