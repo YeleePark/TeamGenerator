adb538094b7d3f690bc08be2743726ebf1e9780a
mzc-yoonp <yoonp@mz.co.kr>
2023-09-13 09:31:39 +0900

    chore : 퍼블리싱 작업중

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import {
   Stack,
   Chip,
 } from "@mui/material";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useForm } from "react-hook-form";
 
 function App() {
@@ -28,7 +28,6 @@ function App() {
 
     setTeams(teams);
   };
-
   const onSubmit = (data) => {
     const list = data?.participantList
       .split("\n")
@@ -42,103 +41,135 @@ function App() {
   };
 
   return (
-    <Container
-      maxWidth={`xl`}
+    <Box
       sx={{
         position: `fixed`,
         width: `100vw`,
         height: `100vh`,
-        display: `flex`,
-        alignItems: `center`,
-        background: `linear-gradient(360deg, rgba(1,106,112,1) 0%, rgba(255,255,221,0.9934567577030813) 70%)`,
+        backgroundColor: `#4158D0`,
+        backgroundImage: `linear-gradient(-30deg, #4158D0 0%, #C850C0 20%, #FFCC70 100%)`,
+        p: 10,
       }}
     >
-      <Paper
-        elevation={2}
+      <Container
+        maxWidth={`xl`}
+        m={`0 auto`}
         sx={{
-          px: 10,
-          py: 4,
-          width: `100%`,
-          height: `80%`,
-          background: `rgba( 255, 255, 255, 0.2 )`,
-          boxShadow: `0 8px 32px 0 rgba( 31, 38, 135, 0.37 )`,
-          backdropFilter: `blur( 5.5px )`,
-          borderRadius: `10px`,
-          border: `1px solid rgba( 255, 255, 255, 0.18 )`,
+          display: `flex`,
+          alignItems: `center`,
+          height: `100%`,
         }}
       >
-        <form
-          onSubmit={handleSubmit(onSubmit)}
-          action=""
-          onKeyDown={(e) => e.stopPropagation()}
+        <Paper
+          elevation={2}
+          sx={{
+            px: 10,
+            py: 4,
+            width: `100%`,
+            height: `100%`,
+            background: `rgba( 255, 255, 255, 0.1 )`,
+            boxShadow: `0 2px 30px 0 rgba(0,0,0,0.3)`,
+            backdropFilter: `blur(5.5px)`,
+            borderRadius: `10px`,
+            overflowY: `scroll`,
+          }}
         >
-          <Grid container spacing={4} rowSpacing={10}>
-            <Grid item xs={12} md={12} textAlign={`center`}>
-              <Typography
-                variant="h6"
-                fontWeight={`bold`}
-                sx={{ position: `absolute` }}
-              >
-                Random Team Generator
-              </Typography>
-            </Grid>
-            <Grid item xs={12} md={6}>
-              <Stack>
-                <TextField
-                  type={`number`}
-                  label={`Number of Teams`}
-                  placeholder={"Enter the number of teams"}
-                  sx={{ mb: 2 }}
-                  InputLabelProps={{ shrink: true }}
-                  inputProps={{ min: 0 }}
-                  {...register(`numberOfTeams`, {
-                    min: 2,
-                    required: true,
-                  })}
-                />
-                <TextField
-                  label={`Participant List`}
-                  multiline
-                  rows={10}
-                  name={`participantList`}
-                  placeholder="Enter participant names (separated by Enter)"
-                  InputLabelProps={{ shrink: true }}
-                  {...register(`participantList`, { required: true })}
-                />
-              </Stack>
-            </Grid>
-            <Grid item xs={12} md={6}>
-              <Box>
-                <Button
-                  variant="contained"
-                  size={`large`}
-                  type={`submit`}
-                  color={`primary`}
+          <form
+            onSubmit={handleSubmit(onSubmit)}
+            action=""
+            onKeyDown={(e) => e.stopPropagation()}
+          >
+            <Grid container spacing={4} rowSpacing={10}>
+              <Grid item xs={12} md={12} textAlign={`center`}>
+                <Typography
+                  variant="h6"
+                  fontWeight={`bold`}
+                  color={`rgba(255,255,255,0.7)`}
+                  sx={{ position: `absolute`, backdropFilter: `blue(5px)` }}
                 >
-                  Generate
-                </Button>
-                <Button
-                  onClick={handleReset}
-                  variant="contained"
-                  size={`large`}
-                  color={`secondary`}
+                  Random Team Generator
+                </Typography>
+              </Grid>
+              <Grid item xs={12} md={4}>
+                <Stack
+                  sx={{
+                    p: 5,
+                    background: `rgba( 255, 255, 255, 0.1 )`,
+                    boxShadow: `0 1px 30px 0 rgba(0,0,0,0.2)`,
+                    borderRadius: `10px`,
+                  }}
                 >
-                  Reset
-                </Button>
-                {teams.map((team, index) => (
-                  <Box key={index} display={`flex`}>
-                    <Typography>Team {index + 1}</Typography>
-                    {team.map((participant) => (
-                      <Chip key={participant} label={participant} />
-                    ))}
-                  </Box>
-                ))}
-              </Box>
+                  <TextField
+                    variant="standard"
+                    type={`number`}
+                    label={`Number of Teams`}
+                    placeholder={"Enter the number of teams"}
+                    sx={{
+                      mb: 2,
+                      background: `rgba(255, 255, 255, 0.2)`,
+                      boxShadow: ` 0 5px 15px rgba(0, 0, 0, 0.05)`,
+                      p: 1,
+                      borderRadius: `5px`,
+                    }}
+                    InputLabelProps={{ shrink: true }}
+                    inputProps={{ min: 0 }}
+                    {...register(`numberOfTeams`, {
+                      min: 2,
+                      required: true,
+                    })}
+                  />
+                  <TextField
+                    label={`Participant List`}
+                    multiline
+                    rows={10}
+                    name={`participantList`}
+                    placeholder="Enter participant names (separated by Enter)"
+                    InputLabelProps={{ shrink: true }}
+                    {...register(`participantList`, { required: true })}
+                  />
+                </Stack>
+              </Grid>
+              <Grid item xs={12} md>
+                <Box
+                  sx={{
+                    p: 5,
+                    height: `100%`,
+                    background: `rgba( 255, 255, 255, 0.1 )`,
+                    boxShadow: `0 1px 30px 0 rgba(0,0,0,0.2)`,
+                    borderRadius: `10px`,
+                  }}
+                >
+                  <Button
+                    variant="contained"
+                    size={`large`}
+                    type={`submit`}
+                    color={`primary`}
+                  >
+                    Generate
+                  </Button>
+                  <Button
+                    onClick={handleReset}
+                    variant="contained"
+                    size={`large`}
+                    color={`secondary`}
+                  >
+                    Reset
+                  </Button>
+                  {teams.map((team, index) => (
+                    <Box key={index} display={`flex`}>
+                      <Typography>Team {index + 1}</Typography>
+                      {team.map((participant) => (
+                        <Chip key={participant} label={participant} />
+                      ))}
+                    </Box>
+                  ))}
+                </Box>
+              </Grid>
             </Grid>
-          </Grid>
-        </form>
-      </Paper>
-    </Container>
+          </form>
+        </Paper>
+      </Container>
+    </Box>
   );
 }
 
